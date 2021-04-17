5759bf6902e30551d9225c02ba41a170ddeb94dc
Meiyo40 <loic.demesmay@gmail.com>
2021-04-17 12:19:17 +0200

    Fix small display issue of ajaxmessage

--- src/main/resources/static/assets/js/ajaxmessage.js
+++ src/main/resources/static/assets/js/ajaxmessage.js
@@ -3,6 +3,7 @@ function ajaxMessage(state, message) {
         let success = document.getElementById("ajaxsuccess");
         success.style.display = "flex";
         success.innerText = message;
+        success.style.left = Math.ceil(document.body.clientWidth/2) - Math.ceil(success.offsetWidth/2) + "px";
         setTimeout( () => {
             success.style.display = "none";
         }, 4000)
@@ -10,6 +11,7 @@ function ajaxMessage(state, message) {
         let fail = document.getElementById("ajaxerror");
         fail.style.display = "flex";
         fail.innerText = message;
+        fail.style.left = Math.ceil(document.body.clientWidth/2) - Math.ceil(fail.offsetWidth/2) + "px";
         setTimeout( () => {
             fail.style.display = "none";
         }, 4000)
@@ -17,6 +19,7 @@ function ajaxMessage(state, message) {
         let warning = document.getElementById("ajaxwarning");
         warning.style.display = "flex";
         warning.innerText = message;
+        warning.style.left = Math.ceil(document.body.clientWidth/2) - Math.ceil(warning.offsetWidth/2) + "px";
         setTimeout( () => {
             warning.style.display = "none";
         }, 4000)
